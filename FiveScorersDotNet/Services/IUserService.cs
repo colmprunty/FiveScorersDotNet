8e02a59e2b251bc8858fbe27f940797ad92fcfe2
colmprunty <colmpruntysignups+github@gmail.com>
2019-01-12 14:21:17 +0000

    start authentication work

---
+++ FiveScorersDotNet/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using FiveScorersDotNet.Models;
+using System.Collections.Generic;
+
+namespace FiveScorersDotNet.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+    }
+}
